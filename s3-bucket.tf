f06cc4d761bf1b2e93b183aadf93626156568a36
Mark Buensalido <mark.buensalido@hashicorp.com>
2022-05-23 00:40:09 +0000

    Added s3 bucket module

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,5 +1,5 @@
 module "s3_bucket" {
-  source = "terraform-aws-modules/s3-bucket/aws"
+  source = "buenospot/terraform-aws-s3-bucket"
 
   bucket_prefix = "${var.prefix}"
   bucket = "my-s3-bucket"
